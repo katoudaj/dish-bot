e4c44215683f0175e8557d8268d118f0e6ab5dfb
katoudaj <katoudaj@gmail.com>
2025-09-04 00:00:45 +0900

    楽天レシピAPIからレシピを取得する Get recipes from RakutenRecipe's API

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-import os
+import os, random, requests
 from dotenv import load_dotenv
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
-import random
 
 load_dotenv()
 ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
-
-menus = ["カレーライス", "ハンバーグ", "豚の生姜焼き", "サバの味噌煮", "オムライス"]
-menu_today = random.choice(menus)
+RAKUTEN_APP_ID = os.getenv("RAKUTEN_APP_ID")
 
 bot = LineBotApi(ACCESS_TOKEN)
-bot.broadcast(TextSendMessage(text=f"今日のおすすめメニューは{menu_today}！"))
+
+def fetch_recipe():
+    url = "https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426"
+    params = {"applicationId": RAKUTEN_APP_ID,
+              "categoryId": 38} # 38は今日の献立のカテゴリID
+    res = requests.get(url, params=params).json()
+    return random.choice(res.get("result", []))
+
+recipe = fetch_recipe()
+text = f"今日のおすすめメニュー：\n{recipe['recipeTitle']}\n{recipe['recipeUrl']}"
+#bot.broadcast(TextSendMessage(text=text))
